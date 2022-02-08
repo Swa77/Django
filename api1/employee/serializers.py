eacf04a6f2a1b4ce5920f5007a9aeb013cbd7e18
Swa77 <77635365+Swa77@users.noreply.github.com>
2022-02-08 20:48:12 +0530

    Delete api1 directory

--- api1/employee/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-class EmployeeSerializer(serializers.Serializer):
-     name=serializers.CharField(max_length=50)
-     
-     company=serializers.CharField(max_length=50)
--- api1/flightDetails/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-#everything after 8000/ is a pattern and we should give here through urlpatterns
-
-urlpatterns=[
-path('Flightdetails/',views.index,name='index'),
-]
